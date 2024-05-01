5eb8f0c81f4b4534ba6081fe2206fad3b47834e7
Alen Mangattu <alenmangattu7@gmail.com>
2024-05-01 19:54:39 +0200

    Conessione al database

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Hello World
+Chatting app in c
+
+sudo pacman -S ncurses lib32-ncurses postgresql postgresql-libs openssl lib32-openssl
--- server/main.c
+++ server/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <stdint.h>
+
+const int LEN = 50;
 
 int main() {
-  printf("Hello World server");
+
+    char dbname[LEN], user[LEN], host[LEN];
+    const char* passwd = malloc(sizeof(char) * LEN);
+    uint16_t port;
+
+    getDbInfo(dbname, user, passwd, host, &port);
+    if (connectDb(dbname, user, passwd, host, port) == -1) {
+        printf("Impossible accedere al database");
+        return 0;
+    };
+    free(passwd);
 }
